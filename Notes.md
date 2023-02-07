aff4157e5adec8a1c56c256e5925a86093e34764
pheldu95 <phelps.duncan@gmail.com>
2023-02-06 22:22:19 -0600

    finished episode 2

---
+++ Notes.md
@@ -0,0 +1,7 @@
+Learned about:
+Model classes (Ship, BattleResult) hold data
+    - classes that hold data, but don't do much work
+    - can be created whenever you need them
+Service Classes (PDO, BattleManager, ShipLoader) do work
+    -do work
+    -Should be created in a centralized location, like Container.php
--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__.'/lib/Ship.php';
-require_once __DIR__.'/lib/BattleManager.php';
-require_once __DIR__.'/lib/ShipLoader.php';
-require_once __DIR__.'/lib/BattleResult.php';
-require_once __DIR__.'/lib/Container.php';
+require_once __DIR__.'/lib/Model/Ship.php';
+require_once __DIR__.'/lib/Service/BattleManager.php';
+require_once __DIR__.'/lib/Service/ShipLoader.php';
+require_once __DIR__.'/lib/Model/BattleResult.php';
+require_once __DIR__.'/lib/Service/Container.php';
 
 //all our database credentials. need them in index.php so we can pass them to ShipLoader
 $configuration = array(
